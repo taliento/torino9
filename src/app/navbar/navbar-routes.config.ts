bef3a7a2701c7275dd4bdb9455f098870a5effb7
Davide <davidetaliento@gmail.com>
2017-09-24 11:30:30 +0200

    lang

--- src/app/navbar/navbar-routes.config.ts
+++ src/app/navbar/navbar-routes.config.ts
@@ -2,8 +2,8 @@ import { MenuType, RouteInfo } from './navbar.metadata';
 
 export const ROUTES: RouteInfo[] = [
   { path: '', title: 'Torino9', menuType: MenuType.BRAND },
-  { path: 'news', title: 'News', menuType: MenuType.LEFT },
-  { path: 'calendar', title: 'Calendar', menuType: MenuType.LEFT },
-  { path: 'about', title: 'About Us', menuType: MenuType.RIGHT },
-  { path: 'contact', title: 'Contact', menuType: MenuType.RIGHT }
+  { path: 'news', title: 'Novità', menuType: MenuType.LEFT },
+  { path: 'calendar', title: 'Calendario', menuType: MenuType.LEFT },
+  { path: 'about', title: 'Su di noi', menuType: MenuType.RIGHT },
+  { path: 'contact', title: 'Contatti', menuType: MenuType.RIGHT }
 ];
