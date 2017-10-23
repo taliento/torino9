9687030c482c6b2a137af106b77f89c76dcf551a
Davide Taliento <davidetaliento@gmail.com>
2017-10-23 17:46:47 +0200

    public img public

--- services/upload.service.js
+++ services/upload.service.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const Q = require('q');
 const publicImgPath = config.publicImgPath;
 
-
 var service = {};
 service.insert = insert;
 service.update = update;
@@ -15,22 +14,17 @@ module.exports = service;
 
 function insert(imgFile) {
   var deferred = Q.defer();
-
-  let imgPath = __dirname + '/..'+ publicImgPath + imgFile.name;
-
+  let imgPath = __dirname + '/..'+ publicImgPath + new Date().getTime() +imgFile.name ;//timestamp name
   imgFile.mv(imgPath, function(err) {
     if (err) deferred.reject(err.name + ': ' + err.message);
     deferred.resolve(publicImgPath + imgFile.name);
   });
-
   return deferred.promise;
 }
 
 function update(imgFile, imagePath) {
   var deferred = Q.defer();
-
   var oldPath = __dirname + '/..' + imagePath;
-
   _delete(oldPath).then(function () {
     insert(imgFile).then(function(path) {
       deferred.resolve(path);
@@ -41,13 +35,11 @@ function update(imgFile, imagePath) {
   .catch(function (err) {
     res.status(400).send(err);
   });
-
   return deferred.promise;
 }
 
 function _delete(imgPath) {
   var deferred = Q.defer();
-
   if (fs.existsSync(imgPath)) {
     fs.unlink(imgPath, function(err) {
       if (err) deferred.reject(err.name + ': ' + err.message);
@@ -56,6 +48,5 @@ function _delete(imgPath) {
   } else {
     deferred.resolve();
   }
-
   return deferred.promise;
 }
