6ec358349066e4ec797d2792a55d847b2db1b18a
Davide Taliento <davidetaliento@gmail.com>
2017-10-27 14:30:05 +0200

    fixing

---
+++ index.js
@@ -0,0 +1,12 @@
+const throng = require('throng');
+
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
+
+function start () {
+  require('./server');
+}
