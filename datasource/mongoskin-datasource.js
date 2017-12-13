b60e2b45e0ad8e76637645d0a9771f86cc7bab19
Davide Taliento <davidetaliento@gmail.com>
2017-12-13 16:51:37 +0100

    fixing

--- datasource/mongoskin-datasource.js
+++ datasource/mongoskin-datasource.js
@@ -1,14 +1,12 @@
+/* jshint node: true */
+'use strict';
+
 const mongo = require('mongoskin');
 //db connection
 const db = mongo.db(process.env.MONGODB_URI, { native_parser: true });
-db.bind('about');
-db.bind('config');
-db.bind('branca');
-db.bind('calendar');
-db.bind('carousel');
-db.bind('contact');
-db.bind('page');
-db.bind('featurette');
-db.bind('news');
-db.bind('users');
+const collections = ['about','config','branca','calendar','carousel','contact','page','featurette','news','users'];
+
+for(let i = 0 ; i < collections.length ; i++) {
+  db.bind(collections[i]);
+}
 module.exports = db;
--- routes/api-mapping.js
+++ routes/api-mapping.js
@@ -1,3 +1,6 @@
+/* jshint node: true */
+'use strict';
+
 const apiMapping = [
   {
     endpoint: '/api/users',
