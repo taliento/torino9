36b0b45ead78ab43c609eb8ed00d3487a8c7d708
Davide Taliento <davidetaliento@gmail.com>
2017-12-13 18:10:11 +0100

    new navbar style

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-layout',
-  templateUrl: './main-layout.component.html'
+  templateUrl: './main-layout.component.html',
+  styleUrls:['./main-layout.component.scss']
 })
 export class MainLayoutComponent {
 
